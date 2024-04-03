ad8a64d3aca7c269ec7985dc19a06640b7d987c3
Kyrie Lin <kyrielin66@gmail.com>
2024-04-02 22:46:13 -0700

    implement java SegmentTree

--- SegmentTree/MySegmentTree.java
+++ SegmentTree/MySegmentTree.java
@@ -3,13 +3,36 @@ package SegmentTree;
 public class MySegmentTree<E> {
     private E[] data;
     private E[] tree;
+    private Merger<E> merger;
 
-    public MySegmentTree(E[] arr) {
-        data = (E[])new Object[arr.length];
+    public MySegmentTree(E[] arr, Merger<E> merger) {
+        this.merger = merger;
+
+        data = (E[]) new Object[arr.length];
         for (int i = 0; i < arr.length; i++)
             data[i] = arr[i];
 
-        tree = (E[])new Object[4 * arr.length];
+        tree = (E[]) new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length - 1);
+    }
+
+    // Build the segment tree
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+
+        // Prevent overflow
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        // Merge the left and right subtrees
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
     public int getSize() {
@@ -18,7 +41,7 @@ public class MySegmentTree<E> {
 
     public E get(int index) {
         if (index < 0 || index >= data.length)
-            throw  new IllegalArgumentException("Index is illegal.");
+            throw new IllegalArgumentException("Index is illegal.");
 
         return data[index];
     }
@@ -29,5 +52,30 @@ public class MySegmentTree<E> {
 
     private int rightChild(int index) {
         return 2 * index + 2;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append('[');
+        for (int i = 0; i < tree.length; i++) {
+            if (tree[i] != null)
+                res.append(tree[i]);
+            else
+                res.append("null");
+
+            if (i != tree.length - 1)
+                res.append(", ");
+        }
+        res.append(']');
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {-2, 0, 3, -5, 2, -1};
+        MySegmentTree<Integer> segTree = new MySegmentTree<>(nums, (a, b) -> a + b);
+
+        System.out.println(segTree.getSize());
+        System.out.println(segTree.get(0));
     }
 }
